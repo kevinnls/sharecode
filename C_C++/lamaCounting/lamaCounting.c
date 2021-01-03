b2905b798dac5a81f1f1322f09e537fae48fd273
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-01-03 20:19:59 +0530

    handle end<start

--- C_C++/lamaCounting/lamaCounting.c
+++ C_C++/lamaCounting/lamaCounting.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 
+int startCount, endCount = 0;
+void killLlamas();
+
 int main() {
+    printf("how many llamas do you have? ");
+    scanf("%d", &startCount);
+    printf("how many llamas do you want? ");
+    scanf("%d", &endCount);
+
+    if(endCount<startCount){
+	killLlamas();
+    }
     return 0;
 }
+
+void killLlamas(){
+    printf("\nyou want %d llamas", endCount);
+    printf("\ngo kill/sell %d llamas.", startCount-endCount);
+    printf("\nif you're quick, you need only 1 day!☠️\n");
+}
+
