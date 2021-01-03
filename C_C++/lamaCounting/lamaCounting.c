5cc128dafae4421a3390e300a77ce60ece8342e5
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-01-03 20:40:13 +0530

    By works, I meant 'doesnt work'.  Works now..
    commits by http://whatthecommit.com/

--- C_C++/lamaCounting/lamaCounting.c
+++ C_C++/lamaCounting/lamaCounting.c
@@ -1,23 +1,42 @@
 #include<stdio.h>
 
-int startCount, endCount = 0;
+int years, startCount, endCount = 0;
+void wantPrompt();
 void killLlamas();
+void keepLlamas();
+int countYears(int start, int end);
 
 int main() {
+
+    welcomeMenu();
+
+    if(endCount<startCount){
+	killLlamas();
+    } else if(endCount==startCount){
+	keepLlamas();
+    } else {
+	years=countYears(startCount, endCount);
+    }
+
+    return 0;
+}
+
+void welcomeMenu(){
+    printf("hello rugged shepherd! i will count the years you need to raise your llama");
     printf("how many llamas do you have? ");
     scanf("%d", &startCount);
     printf("how many llamas do you want? ");
     scanf("%d", &endCount);
-
-    if(endCount<startCount){
-	killLlamas();
-    }
-    return 0;
 }
-
-void killLlamas(){
+void wantPrompt(){
     printf("\nyou want %d llamas", endCount);
+}
+void killLlamas(){
+    wantPrompt();
     printf("\ngo kill/sell %d llamas.", startCount-endCount);
     printf("\nif you're quick, you need only 1 day!☠️\n");
 }
-
+void keepLlamas(){
+    wantPrompt();
+    printf("you already reached your target");
+}
