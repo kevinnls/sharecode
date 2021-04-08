66df26b9129675d9089262ffb9ae58b9073a8375
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-04-08 13:22:17 +0530

    C: trying to see effect of header files in binary size

---
+++ C_C++/compiledBinSizes/threeFunc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world");
+    puts("hello world");
+    writeline("helloworld");
+    return 0;
+}
---
+++ C_C++/compiledBinSizes/twoFunc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world");
+    puts("hello world");
+    return 0;
+}
