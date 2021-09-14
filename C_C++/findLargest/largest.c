ed50fcbdd7bf656cdd2f2b6e67c5508b1a2c86b8
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-14 16:10:53 +0530

    [C/C++](largest) working

--- C_C++/findLargest/largest.c
+++ C_C++/findLargest/largest.c
@@ -1,47 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void printIntArray(int *arr);
-void printCharArray(char **arr);
-
-int findLargest (int *arr) {
-    int aggLarge;
-    for (int i = 0 ; i < sizeof(arr) ; i++) {
-	if( arr[i] > arr[i+1]) {
-	    aggLarge = arr[i];
-	} else {
-	    aggLarge = arr[i+1];
-	}
-    }
-    return aggLarge;
-}
-void integeriseArgv (int len, char **argv, int *memloc) {
-    for(int i=0; i < len-1; i++){
-	memloc[i] = atoi(argv[i+1]);
-    }
-    return;
-}
 int main(int argc, char **argv) {
     if(argc > 1) {
-	int *arr;
-	integeriseArgv(argc, argv, arr);
-	//findlargest(arr)
+	int aggLarge = 0;
+	for (int i = 0 ; i < argc-1 ; i++) {
+	    int num = atoi(argv[i+1]);
+	    if ( num > aggLarge ) {
+		aggLarge = num;
+	    }
+	}
+	printf("%d\n", aggLarge);
 	return 0;
     } else {
 	fprintf(stderr, "ERR: no arguments provided\n");
 	return 1;
-    }
-}
-
-void printIntArray(int *arr){
-    int len = sizeof(arr);
-    for (int i=0; i<len; i++){
-	printf("%d", arr[i]);
-    }
-}
-void printCharArray(char **arr){
-    int len = sizeof(arr);
-    for (int i=0; i<len; i++){
-	printf("%s", arr[i]);
     }
 }
