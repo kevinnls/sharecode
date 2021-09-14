b4489d956c80aa5c598235fa669e829d9c4ed002
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-14 16:14:50 +0530

    [C/C++](largest) add support for negative integers

--- C_C++/findLargest/largest.c
+++ C_C++/findLargest/largest.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 
 int main(int argc, char **argv) {
     if(argc > 1) {
-	int largest = 0;
+	int largest = INT_MIN;
 	for (int i = 0 ; i < argc-1 ; i++) {
 	    int num = atoi(argv[i+1]);
 	    if ( num > largest ) {
