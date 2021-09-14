7eb91a996899fefe72c4871d11b2ce5d6e51abae
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-14 13:23:08 +0530

    [C/C++](findLargest): indent and some stuff

--- C_C++/findLargest/largest.c
+++ C_C++/findLargest/largest.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int findLargest (int *arr) {
-  int aggLarge;
-  for (int i = 0 ; i < sizeof(arr) ; i++) {
-    if( arr[i] > arr[i+1]) {
-      aggLarge = arr[i];
-    } else {
-      aggLarge = arr[i+1];
+    int aggLarge;
+    for (int i = 0 ; i < sizeof(arr) ; i++) {
+	if( arr[i] > arr[i+1]) {
+	    aggLarge = arr[i];
+	} else {
+	    aggLarge = arr[i+1];
+	}
     }
-  }
-  return aggLarge;
+    return aggLarge;
 }
 void integeriseArgv (int len, char **argv, int *memloc) {
-  for(int i=0; i < len-1; i++){
-    memloc[i] = atoi(argv[i+1]);
-  }
-  return;
+    for(int i=0; i < len-1; i++){
+	memloc[i] = atoi(argv[i+1]);
+    }
+    return;
 }
 int main(int argc, char **argv) {
-  int *arr;
-  printf("%d",argc);
-  return 0;
- integeriseArgv(argc, argv, arr);
-  for(int i = 0; i < argc; i++)
-  printf("%d\n", arr[i]);
-  return 0;
+    if(argc > 1) {
+	int *arr;
+	integeriseArgv(argc, argv, arr);
+	//findlargest(arr)
+    } else {
+	fprintf(stderr, "ERR: no arguments provided\n");
+	return 1;
+    }
 }
