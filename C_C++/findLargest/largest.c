f068818bf05878bbf1bcdbbdbbf780ac4878edce
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-14 17:20:09 +0530

    more standard C formatting

--- C_C++/findLargest/largest.c
+++ C_C++/findLargest/largest.c
@@ -1,20 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int main(int argc, char **argv) {
-    if(argc > 1) {
-	int largest = INT_MIN;
-	for (int i = 1 ; i < argc ; i++) {
-	    int num = atoi(argv[i]);
-	    if ( num > largest ) {
-		largest = num;
-	    }
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		int largest = INT_MIN;
+		for (int i = 1; i < argc; i++)
+		{
+			int num = atoi(argv[i]);
+			if (num > largest)
+			{
+				largest = num;
+			}
+		}
+		printf("%d\n", largest);
+		return 0;
 	}
-	printf("%d\n", largest);
-	return 0;
-    } else {
-	fprintf(stderr, "ERR: no arguments provided\n");
-	return 1;
-    }
+	else
+	{
+		fprintf(stderr, "ERR: no arguments provided\n");
+		return 1;
+	}
 }
