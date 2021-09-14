48b420e7ecc08350b0f711d8c3943b8cbebb78bf
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-14 13:42:05 +0530

    [C/C++](findlargest): debug functions

--- C_C++/findLargest/largest.c
+++ C_C++/findLargest/largest.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void printArray(int *arr);
+void printArray(int **char);
+
 int findLargest (int *arr) {
     int aggLarge;
     for (int i = 0 ; i < sizeof(arr) ; i++) {
@@ -23,8 +26,22 @@ int main(int argc, char **argv) {
 	int *arr;
 	integeriseArgv(argc, argv, arr);
 	//findlargest(arr)
+	return 0;
     } else {
 	fprintf(stderr, "ERR: no arguments provided\n");
 	return 1;
+    }
+}
+
+void printArray(int *arr){
+    int len = sizeof(arr);
+    for (int i=0; i<len; i++){
+	printf("%d", arr[i]);
+    }
+}
+void printArray(char **arr){
+    int len = sizeof(arr);
+    for (int i=0; i<len; i++){
+	printf("%s", arr[i]);
     }
 }
