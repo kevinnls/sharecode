5b3781368c55a09741c29cc2c1477a02d3dabf91
kevinnls <developer@kevinnlsamuel.com>
2022-05-16 09:51:01 +0200

    format with indent(1)

--- C_C++/countDigitsInInteger/convertingToCharArr/main.c
+++ C_C++/countDigitsInInteger/convertingToCharArr/main.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main( int argc, char** argv ) {
-    if ( argc < 2 ) return 5;
+int
+main (int argc, char **argv)
+{
+  if (argc < 2)
+    return 5;
 
-    for (int i=1 ; i < argc ; i++) {
-    int integer = atoi(argv[i]) ;
-    char buffer[BUFSIZ];
-    sprintf( buffer, "%d", integer );
-    printf("%d is %lu digits long\n", integer, strlen(buffer));
+  for (int i = 1; i < argc; i++)
+    {
+      int integer = atoi (argv[i]);
+      char buffer[BUFSIZ];
+      sprintf (buffer, "%d", integer);
+      printf ("%d is %lu digits long\n", integer, strlen (buffer));
     }
 }
